2b04f2d0569878781663006329a1402bd3b6e219
Fernanda Silverio <20233005420@estudantes.ifpr.edu.br>
2025-06-30 19:32:38 -0300

    FINALIZADO

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const cepInput = document.getElementById('cep');
-const erro = document.getElementById('erro');
 
 cepInput.addEventListener('input', () => {
   const cep = cepInput.value.replace(/\D/g, '');
@@ -9,13 +8,10 @@ cepInput.addEventListener('input', () => {
 });
 
 function buscarEndereco(cep) {
-  erro.classList.add('hidden');
-
   fetch(`https://viacep.com.br/ws/${cep}/json/`)
     .then(res => res.json())
     .then(data => {
       if (data.erro) {
-        mostrarErro('CEP não encontrado.');
         limparCampos();
         return;
       }
@@ -28,19 +24,12 @@ function buscarEndereco(cep) {
       document.getElementById('complemento').value = data.complemento || '';
     })
     .catch(() => {
-      mostrarErro('Erro ao consultar o CEP.');
       limparCampos();
     });
 }
 
-function mostrarErro(msg) {
-  erro.textContent = msg;
-  erro.classList.remove('hidden');
-  limparCampos();
-}
-
 function limparCampos() {
-  const campos = ['logradouro', 'bairro', 'cidade', 'uf', 'codigo_ibge']
+  const campos = ['logradouro', 'bairro', 'cidade', 'uf', 'codigo_ibge', 'complemento'];
   campos.forEach(id => {
     const el = document.getElementById(id);
     if (el) el.value = '';
